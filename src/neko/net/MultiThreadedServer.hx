1dc2f0648bc7356d51924f76635db55004329dc1
Sam MacPherson <xylom44@gmail.com>
2013-03-22 19:44:08 -0400

    missing dependency; io workers no longer round robin - switched to random

--- src/neko/net/MultiThreadedServer.hx
+++ src/neko/net/MultiThreadedServer.hx
@@ -1,3 +1,13 @@
+/****
+* Copyright (C) 2013 Sam MacPherson
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+****/
+
 package neko.net;
 
 import haxe.io.Bytes;
@@ -43,7 +53,6 @@ class MultiThreadedServer < Client, Message > {
 	var timer:Thread;
 	var ioThreads:Array<Thread>;
 	var workerThreads:Array<Thread>;
-	var nextIoThread:CyclicCounter;
 	var mainThread:Thread;
 	
 	//Mutable objects
@@ -87,7 +96,6 @@ class MultiThreadedServer < Client, Message > {
 		serv.bind(host, port);
 		serv.listen(numListen);
 		clients.push(serv);
-		nextIoThread = new CyclicCounter(0, numIoThreads);
 		timer = Thread.create(runTimer);
 		for (i in 0 ... numIoThreads) {
 			ioThreads.push(Thread.create(runWorker));
@@ -215,7 +223,7 @@ class MultiThreadedServer < Client, Message > {
 	}*/
 	
 	function doIoWork (f:Void -> Void):Void {
-		ioThreads[nextIoThread.getNext()].sendMessage(f);
+		ioThreads[Std.int(Math.random() * ioThreads.length)].sendMessage(f);
 	}
 	
 	function doApplicationWork (sock:Socket, f:Void -> Void):Void {
