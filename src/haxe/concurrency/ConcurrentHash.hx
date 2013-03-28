9f18a6b52ec7f24ff733924dbfd9308b4f467def
Sam MacPherson <xylom44@gmail.com>
2013-03-28 18:39:31 -0400

    added datastructures; renamed library

---
+++ src/haxe/concurrency/ConcurrentHash.hx
@@ -0,0 +1,105 @@
+/****
+* Copyright (C) 2013 Sam MacPherson
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+****/
+
+package haxe.concurrency;
+
+#if neko
+import neko.vm.Mutex;
+#elseif cpp
+import cpp.vm.Mutex;
+#end
+
+/**
+ * A simple wrapper for using the Hash. Access is thread safe.
+ * 
+ * @author Sam MacPherson
+ */
+
+class ConcurrentHash<T> {
+	
+	var lock:Mutex;
+	var hash:Hash<T>;
+
+	public function new () {
+		lock = new Mutex();
+		hash = new Hash<T>();
+	}
+	
+	public inline function exists (key:String):Bool {
+		lock.acquire();
+		var result = hash.exists(key);
+		lock.release();
+		return result;
+	}
+	
+	public inline function get (key:String):Null<T> {
+		lock.acquire();
+		var result = hash.get(key);
+		lock.release();
+		return result;
+	}
+	
+	public inline function iterator ():Iterator<T> {
+		var arr = new Array<T>();
+		lock.acquire();
+		for (i in hash.iterator()) {
+			arr.push(i);
+		}
+		lock.release();
+		return arr.iterator();
+	}
+	
+	public inline function keys ():Iterator<String> {
+		var arr = new Array<String>();
+		lock.acquire();
+		for (i in hash.keys()) {
+			arr.push(i);
+		}
+		lock.release();
+		return arr.iterator();
+	}
+	
+	public inline function remove (key:String):Bool {
+		lock.acquire();
+		var result = hash.remove(key);
+		lock.release();
+		return result;
+	}
+	
+	public inline function set (key:String, val:T):Void {
+		lock.acquire();
+		hash.set(key, val);
+		lock.release();
+	}
+	
+	public inline function toString ():String {
+		lock.acquire();
+		var result = hash.toString();
+		lock.release();
+		return result;
+	}
+	
+	public inline function setIfNotExists (key:String, val:T):Bool {
+		lock.acquire();
+		var bool = !hash.exists(key);
+		if (bool) hash.set(key, val);
+		lock.release();
+		return bool;
+	}
+	
+	public inline function getAndRemove (key:String):T {
+		lock.acquire();
+		var result = hash.get(key);
+		hash.remove(key);
+		lock.release();
+		return result;
+	}
+	
+}
---
+++ src/haxe/concurrency/CopyOnWriteArray.hx
@@ -0,0 +1,122 @@
+/****
+* Copyright (C) 2013 Sam MacPherson
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+****/
+
+
+
+package haxe.concurrency;
+
+/**
+ * A thread safe array. The array is copied completely during a write. Useful for read-heavy usage.
+ * Be very careful when iterating by index as the array length can change during this.
+ * It is a better idea to iterate by the default iterator and store an index variable alongside the iteration.
+ * 
+ * @author Sam MacPherson
+ */
+
+class CopyOnWriteArray<T> {
+	
+	public var length(_length, null):Int;
+	
+	var arr:Array<T>;
+
+	public function new () {
+		arr = new Array<T>();
+	}
+	
+	function _length ():Int {
+		return arr.length;
+	}
+	
+	public function concat (a:Array<T>):Array<T> {
+		return arr.concat(a);
+	}
+	
+	public function copy ():Array<T> {
+		return arr.copy();
+	}
+	
+	public function insert (pos:Int, x:T):Void {
+		var a = new Array();
+		var index = 0;
+		for (i in arr) {
+			if (index == pos) a.push(x);
+			a.push(i);
+			index++;
+		}
+		arr = a;
+	}
+	
+	public function iterator ():Iterator<T> {
+		return arr.iterator();
+	}
+	
+	public function join (sep:String):String {
+		return arr.join(sep);
+	}
+	
+	public function pop ():Null<T> {
+		var a = arr.copy();
+		var e = a.pop();
+		arr = a;
+		return e;
+	}
+	
+	public function push (x:T):Int {
+		var a = arr.copy();
+		var l = a.push(x);
+		arr = a;
+		return l;
+	}
+	
+	public function remove (x:T):Bool {
+		var a = arr.copy();
+		var b = a.remove(x);
+		arr = a;
+		return b;
+	}
+	
+	public function reverse ():Void {
+		var a = arr.copy();
+		a.reverse(x);
+		arr = a;
+	}
+	
+	public function shift ():Null<T> {
+		var a = arr.copy();
+		var e = a.shift();
+		arr = a;
+		return e;
+	}
+	
+	public function slice (pos:Int, ?end:Int):Array<T> {
+		return arr.slice(pos, end);
+	}
+	
+	public function sort (f:T->T->Int):Void {
+		var a = arr.copy();
+		a.sort(f);
+		arr = a;
+	}
+	
+	public function splice (pos:Int, len:Int):Array<T> {
+		return arr.splice(pos, len);
+	}
+	
+	public function toString ():String {
+		return arr.toString();
+	}
+	
+	public function unshift (x:T):Void {
+		var a = arr.copy();
+		a.unshift(x);
+		arr = a;
+	}
+	
+}
