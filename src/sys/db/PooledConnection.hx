d470c1bb9c6613612e3bc20843d52b66fa363297
Sam MacPherson <xylom44@gmail.com>
2013-05-14 18:08:23 -0400

    bugfix

--- src/sys/db/PooledConnection.hx
+++ src/sys/db/PooledConnection.hx
@@ -8,6 +8,8 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ****/
 
+
+
 package sys.db;
 
 #if neko
@@ -95,9 +97,12 @@ class PooledConnection implements Connection {
 				if (connWrapper.ready) {
 					conn = connWrapper.conn;
 					connWrapper.ready = false;
+					break;
 				}
 			}
-			Sys.sleep(0.1);	//Sleep a bit
+			if (conn == null) {
+				Sys.sleep(0.1);	//Sleep a bit
+			}
 		}
 		lock.release();
 		return conn;
